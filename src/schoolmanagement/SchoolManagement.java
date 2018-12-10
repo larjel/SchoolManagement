aab868a7c4f975596a70035259af99154c984ccf
Lars Jelleryd <larsj99@yahoo.se>
2018-12-10 13:13:02 +0100

    First commit

---
+++ src/schoolmanagement/SchoolManagement.java
@@ -0,0 +1,16 @@
+package schoolmanagement;
+
+/**
+ *
+ * @author Lars Jelleryd
+ */
+public class SchoolManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
