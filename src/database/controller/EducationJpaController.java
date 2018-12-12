07306f85aacf943cc7875977c2326a26af22c55b
Lars Jelleryd <larsj99@yahoo.se>
2018-12-12 14:31:36 +0100

    Some basic database/entity handling added.

--- src/database/controller/EducationJpaController.java
+++ src/database/controller/EducationJpaController.java
@@ -1,7 +1,45 @@
 package database.controller;
 
+import database.MyEntityManager;
+import database.domain.Teacher;
+import java.util.List;
 import javax.persistence.*;
 
 public class EducationJpaController {
+
+    private static boolean teacherExists(Teacher teacher) {
+        try {
+            MyEntityManager.get()
+                    .createNamedQuery("Teacher.findByPersonalIdNumber", Teacher.class)
+                    .setParameter("personalIdNumber", teacher.getPersonalIdNumber())
+                    .getSingleResult();
+            return true;
+        } catch (NoResultException e) {
+            return false;
+        }
+    }
+
+    public static void addTeacher(Teacher teacher) {
+        if (teacherExists(teacher)) {
+            throw new RuntimeException("Teacher already exists");
+        }
+
+        final EntityManager em = MyEntityManager.get();
+        final EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.persist(teacher);
+            tx.commit();
+        } catch (RuntimeException e) {
+            MyEntityManager.rollback(tx);
+            throw e;
+        }
+    }
+
+    public static List<Teacher> getAllTeachers() {
+        return MyEntityManager.get()
+                .createNamedQuery("Teacher.findAll", Teacher.class)
+                .getResultList();
+    }
 
 }
--- src/database/controller/StudentJpaController.java
+++ src/database/controller/StudentJpaController.java
@@ -1,7 +1,40 @@
 package database.controller;
 
+import database.MyEntityManager;
+import database.domain.Student;
+import database.domain.Teacher;
 import javax.persistence.*;
 
 public class StudentJpaController {
+
+    private static boolean studentExists(Student student) {
+        try {
+            MyEntityManager.get()
+                    .createNamedQuery("Student.findByPersonalIdNumber", Student.class)
+                    .setParameter("personalIdNumber", student.getPersonalIdNumber())
+                    .getSingleResult();
+
+            return true;
+        } catch (NoResultException e) {
+            return false;
+        }
+    }
+
+    public static void addStudent(Student student) {
+        if (studentExists(student)) {
+            throw new RuntimeException("Student already exists");
+        }
+
+        final EntityManager em = MyEntityManager.get();
+        final EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.persist(student);
+            tx.commit();
+        } catch (RuntimeException e) {
+            MyEntityManager.rollback(tx);
+            throw e;
+        }
+    }
 
 }
